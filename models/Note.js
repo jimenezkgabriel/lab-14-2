eaec406a6d4e7000b1b8e99dded6a758e7801615
jimenezkgabriel <jimenez.k.gabriel@gmail.com>
2026-01-21 13:25:42 -0500

    cleaning up some code

--- models/Note.js
+++ models/Note.js
@@ -1,22 +1,27 @@
 const { Schema, model } = require('mongoose');
- 
+
 // This is the model you will be modifying
 const noteSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
 });
- 
+
 const Note = model('Note', noteSchema);
- 
+
 module.exports = Note;
--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const path = require('path');
 const db = require('./config/connection.js');
 const routes = require('./routes');
 require('dotenv').config();
- 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
- 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
- 
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
- 
+
 app.use(routes);
- 
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on http://localhost:${PORT}`));
+    app.listen(PORT, () => console.log(`🌍 Now listening on http://localhost:${PORT}`));
 });
